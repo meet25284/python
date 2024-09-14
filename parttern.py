e35b168343c77ceaf4b2c0d4dd037dcae30b62a7
MEET <meetpansuriya93@gmail.com>
2024-09-14 21:38:25 +0530

    comment add

--- parttern.py
+++ parttern.py
@@ -1,10 +1,11 @@
 row=int(input("enter the no. of rows:"))
+# //i took input from user//
 
 for i in range(0,row):
     for j in range(0,i+1):
         print("*",end=" ")
     print("\n")
-        
+
 
 
 
