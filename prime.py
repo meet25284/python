36d11920c19bdf90e52b8a11c3716d2129c633d8
MEET <meetpansuriya93@gmail.com>
2024-09-22 22:54:00 +0530

    add comments and object type function

--- prime.py
+++ prime.py
@@ -1,8 +1,10 @@
-num = int(input("Enter any number: "))
+num = int(input("Enter any number: "))#first takes input in int value
 
-if num % 2 == 0 or num % 3 == 0 or num % 5 == 0 or num % 7 == 0:
+if num % 2 == 0 or num % 3 == 0 or num % 5 == 0 or num % 7 == 0:#apply if else condition
     print("It is not a prime number")
 elif num == 1:
     print("1 is not cosider as prime number")
 else :
     print("its a prime number")
+
+print(type(num)) #ask to python for object type   
