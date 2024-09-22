a35b1933f35d9fe82a6d3e917a9a5fcb8d4d7040
MEET <meetpansuriya93@gmail.com>
2024-09-23 00:06:41 +0530

    comment added

---
+++ datatype.py
@@ -0,0 +1,6 @@
+int = 5 #its a int datatype in which positive and negative no can store
+float = 5.6 # in float datatype decimal value can be store
+str = "meet" #in string datatype alphabates can store
+boolean = True # in boolean datatype true and false can store here first letter must be capital
+str ="a" 
+a = None # in none datatype mean no value has assign it
