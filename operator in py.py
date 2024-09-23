7621437ce0b5aebc4687c920fd5cf4e1eff5f01e
MEET <meetpansuriya93@gmail.com>
2024-09-23 21:35:30 +0530

    type of operator

---
+++ operator in py.py
@@ -0,0 +1,9 @@
+a = 19
+b = 10
+#arithmetic operator
+print(a+b)#this is use to sum
+print(a-b)#this is use to sub
+print(a*b)#this is use to multiply
+print(a/b)#this is use to divide
+print(a%b)#this is use to find remaider 
+print(a**b)#this is use find a^b
