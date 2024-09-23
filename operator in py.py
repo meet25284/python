89c9f6250e188ef3fe4fa7717b7b59a82fe16daa
MEET <meetpansuriya93@gmail.com>
2024-09-23 22:43:13 +0530

    add relational and assignment op

--- operator in py.py
+++ operator in py.py
@@ -7,3 +7,29 @@ print(a*b)#this is use to multiply
 print(a/b)#this is use to divide
 print(a%b)#this is use to find remaider 
 print(a**b)#this is use find a^b
+
+
+#relational operator
+print(a==b)#it means a is equal to b
+print(a!=b)#it means a is not equal to b
+print(a>b)#it means a is greater than b
+print(a<b)#it means a is less than b
+print(a>=b)#it means a is greater than equal to b
+print(a<=b)#it means a is less than equal to b
+
+
+
+#assignment operator
+print(a=b)#it mean the value of a is assign in a
+a+=b#it mean the value of a+b is assign in a
+print(a)
+a-=b#it mean the value of a-b is assign in a
+print(a)
+a*=b#it mean the value of a*b is assign in a
+print(a)
+a/=b#it mean the value of a/b is assign in a
+print(a)
+a%=b#it mean the value of a%b is assign in a
+print(a)
+a**=b#it mean the value of a**b is assign in a
+print(a)
