8434a9d556a5904037b3ab3f7f99c7e163d61327
MEET <meetpansuriya93@gmail.com>
2024-10-23 13:27:16 +0530

    swap using user define function

---
+++ swap.py
@@ -0,0 +1,17 @@
+def swap(x, y):
+
+
+
+# Input from the user
+x = int(input("Enter the value of x: "))
+y = int(input("Enter the value of y: "))
+
+# Displaying original values
+print(f"Before swapping: x = {x}, y = {y}")
+
+# Calling the swap function
+x, y = swap(x, y)
+
+# Displaying swapped values
+print(f"After swapping: x = {x}, y = {y}")
+
