391334f85d5498b9bdc52453ab2291d3e7fe6c73
MEET <meetpansuriya93@gmail.com>
2024-12-06 18:40:29 +0530

    right swifted pyramid

--- parttern.py
+++ parttern.py
@@ -1,6 +1,5 @@
-row=int(input("enter the no. of rows:"))
-# //i took input from user//
-for i in range(0,row):
-    for j in range(0,i+1):
-        print("*",end=" ")
-    print("\n")
+row=(int(input("enter no. of value you want to get:"))+1)
+
+for i in range(row,0,-1):
+   
+    print("* " * (row - i))
