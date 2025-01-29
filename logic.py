bbc731929ab14d1078f14c6793725d883d9a342e
MEET <meetpansuriya93@gmail.com>
2025-01-30 00:45:47 +0530

    fizz buzz game

--- logic.py
+++ logic.py
@@ -1,4 +1,17 @@
-#celcius to farenhit converter
-temp=float(input("enter temperature in celcius:"))
-print((temp*9/5)+32)
-print(end="F")
+def fizzBuzz(n):
+    for i in range(1,n+1):
+        if i % 3 == 0 and i % 5 ==0:
+            print("fizzBuzz")
+        
+        elif i % 3 == 0:
+            print("fizz")    
+        
+        elif i % 5 == 0:
+            print("buzz")
+              
+        else:
+            print(i)
+            
+n = int(input("enter number:")) 
+fizzBuzz(n)      
+        
